5ae81ee634ba8ce4b324b657fcd7a5387f7aa706
Alexander Vollschwitz <vollschwitz@gmx.net>
2021-07-26 21:51:01 +0200

    setup Makefile

--- Makefile
+++ Makefile
@@ -22,7 +22,7 @@
 SHELL = /bin/bash
 
 REPO = oqtadrive
-OQTADRIVE_RELEASE = 0.1.4
+OQTADRIVE_RELEASE = 0.1.5-wip
 OQTADRIVE_VERSION := $(shell git describe --always --tag --dirty)
 
 ROOT = $(shell pwd)
--- hack/devenvutil.sh
+++ hack/devenvutil.sh
@@ -42,7 +42,8 @@ function synopsis {
 
     files=()
 
-    command -v gawk > /dev/null || echo "Note: proper help display requires gawk!"
+    command -v gawk > /dev/null || echo \
+        "Note: proper help display requires gawk! (e.g. sudo apt install gawk)"
 
     for file in ${MAKEFILE_LIST}; do
         if [[ "$(basename "${file}")" == "Makefile" ]]; then
@@ -72,7 +73,7 @@ function synopsis {
 
     # other notes
     print_formatted_help "$(cat "${files[@]}" \
-        | gawk '/^##$/{f=1; printf "-%s", $0; next} /^[^#].*$/{f=0} /^$/{f=0} f' \
+        | gawk '/^##$/{f=1; printf "%s", $0; next} /^[^#].*$/{f=0} /^$/{f=0} f' \
         | tr -d '#')"
     echo
 }
@@ -146,6 +147,288 @@ function build_binary {
     else
     	rm -f "../${binary}"
     fi
+}
+
+#
+#
+#
+function download_oqtactl {
+
+    local arch
+    arch="$(get_architecture)" || return 1
+
+    local os
+    os="$(get_os "${arch}")" || return 1
+
+    local marker="${os,,}_${arch}"
+    local url
+
+    if [[ -z "${BUILD_URL}" ]]; then # get form GitHub release page
+        url="$(get_asset_url "${marker}.zip")"
+
+    else # get from custom build page
+        url="$(curl -fsSL "${BUILD_URL}" \
+            | grep "${marker}\.zip" \
+            | cut -d '"' -f 8)" && url="${BUILD_URL}/${url}"
+    fi
+
+    if [[ -z "${url}" || ! ${url} =~ ${marker} ]]; then
+        echo -e \
+            "\nNo download available for architecture '${arch}' on OS '${os}', in version '${VERSION}'.\n" >&2
+        return 1
+    fi
+
+    echo "  from ${url}"
+    curl -fsSL "${url}" -o oqtactl.zip
+    unzip -o oqtactl.zip
+    rm oqtactl.zip
+    chmod +x oqtactl
+    mv oqtactl "${OQTACTL}"
+}
+
+#
+#
+#
+function download_firmware {
+
+    local url
+
+    if [[ -z "${BUILD_URL}" ]]; then # get form GitHub repo
+        [[ -n "${VERSION}" ]] || VERSION="$(get_latest_release)"
+        url="${BASE_URL}/${VERSION}/arduino/oqtadrive.ino"
+
+    else # get from custom build page
+        url="${BUILD_URL}/oqtadrive.ino"
+    fi
+
+    echo "  from ${url}"
+    curl -fsSL  -o "${SKETCH}.org" "${url}"
+}
+
+#
+# $1    filter
+#
+function get_asset_url {
+
+    local path="releases/latest"
+    [[ -z "${VERSION}" || "${VERSION}" == "latest" ]] \
+        || path="releases/tags/${VERSION}"
+
+    github_api_call "${path}" 2>/dev/null \
+        | jq -r ".assets[]
+            | select(.name | contains(\"$1\"))
+            | .browser_download_url"
+}
+
+#
+#
+#
+function get_latest_release {
+    github_api_call "releases/latest" 2>/dev/null \
+        | jq -r ".name"
+}
+
+#
+# $1    path
+#
+function github_api_call {
+    curl -fsSL -H "Accept: application/vnd.github.v3+json" \
+        "https://api.github.com/repos/xelalexv/oqtadrive/$1"
+}
+
+#
+# $...  actions, `install|remove|start|stop|enable|disable`
+#
+function manage_service {
+
+    for a in "$@"; do
+
+        case "${a}" in
+
+            install)
+                curl -fsSL "${BASE_URL}/${BRANCH}/hack/oqtadrive.service" \
+                    | sed -E -e "s;^ExecStart=.*$;ExecStart=${OQTACTL} serve -d ${PORT};g" \
+                          -e "s;^WorkingDirectory=.*$;WorkingDirectory=${ROOT};g" \
+                          -e "s;^User=.*$;User=${USER};g" \
+                    | sudo tee /etc/systemd/system/oqtadrive.service > /dev/null
+                sudo systemctl daemon-reload
+                ;;
+
+            remove)
+                sudo rm --force /etc/systemd/system/oqtadrive.service
+                ;;
+
+            start|stop|status|enable|disable)
+                [[ ! -f /etc/systemd/system/oqtadrive.service ]] || {
+                    echo "running daemon service action: ${a}"
+                    sudo systemctl "${a}" "${OQTADRIVE_SERVICE}"
+                }
+                ;;
+
+            *)
+                echo -e "\nUnknown service command: ${a}\n" >&2
+                return 1
+                ;;
+        esac
+
+        sleep 1
+    done
+}
+
+#
+#
+#
+function patch_avrdude {
+
+    local avrdude
+    avrdude="$(find ~/.arduino15/ -type f -name avrdude.org)"
+    [[ -z "${avrdude}" ]] || {
+        echo "avrdude already patched!"
+        return 1
+    }
+
+    avrdude="$(find ~/.arduino15/ -type f -name avrdude)"
+
+    [[ -n "${avrdude}" ]] || {
+        echo "avrdude not installed!"
+        return 1
+    }
+
+    mv "${avrdude}" "${avrdude}.org"
+
+    local dir
+    dir="$(dirname "${avrdude}")"
+
+    local autoreset="${dir}/autoreset"
+    curl -fsSL -o "${autoreset}" "${BASE_URL}/${BRANCH}/hack/autoreset"
+    sed -Ei "s/^pin[[:space:]]*=[[:space:]]*[0-9]+$/pin = ${RESET_PIN}/g" \
+        "${autoreset}"
+    chmod +x "${autoreset}"
+
+    cat <<EOF > "${avrdude}"
+#!/bin/sh
+sudo strace -o "|${autoreset}" -eioctl "${dir}/avrdude.org" \$@
+EOF
+    chmod +x "${avrdude}"
+}
+
+#
+#
+#
+function unpatch_avrdude {
+
+    local avrdude
+    avrdude="$(find ~/.arduino15/ -type f -name avrdude.org)"
+    [[ -n "${avrdude}" ]] || {
+        echo "avrdude not patched!"
+        return 1
+    }
+
+    local dir
+    dir="$(dirname "${avrdude}")"
+
+    mv --force "${avrdude}" "${dir}/avrdude"
+    rm --force "${dir}/autoreset"
+}
+
+#
+# $...  prerequisites
+#
+function prereqs {
+
+    local missing
+
+    for p in "$@"; do
+        type "${p}" >/dev/null 2>&1 || {
+            missing+=" ${p}"
+        }
+    done
+
+    [[ -z "${missing}" ]] || {
+        echo -e "\nYou need to install these dependencies:${missing}\n"
+        return 1
+    }
+}
+
+#
+#
+#
+function sanity {
+    local arch
+    arch="$(get_architecture)" || return 1
+    get_os "${arch}" > /dev/null || return 1
+}
+
+#
+#
+#
+function get_architecture {
+
+    local arch
+    arch="$(uname -m)"
+
+    case ${arch} in
+        x86_64)
+            echo -n "amd64"
+            ;;
+        x86|i386|i686)
+            echo -n "386"
+            ;;
+        armv5*|armv6*|armv7*)
+            echo -n "arm"
+            ;;
+        aarch64)
+            echo -n "arm64"
+            ;;
+        *)
+            echo -e "\nUnsupported architecture: ${arch}\n" >&2
+            return 1
+            ;;
+    esac
+}
+
+#
+# $1    architecture
+#
+function get_os {
+
+    local os
+    os="$(uname -s)"
+
+    local err=1
+
+    case ${os} in
+
+        Linux)
+            case $1 in
+                amd64|amd|arm|386)
+                    err=0
+                    ;;
+            esac
+            ;;
+
+        Darwin)
+            case $1 in
+                amd64|arm64)
+                    err=0
+                    ;;
+            esac
+            echo -e "\nWARNING: This has not been tested on MacOS yet!\n" >&2
+            ;;
+
+        *)
+            echo -e "\n${os} not supported.\n" >&2
+            return 1
+            ;;
+    esac
+
+    [[ ${err} -eq 0 ]] || \
+        {
+            echo -e "\nArchitecture $1 not supported on ${os}.\n" >&2
+            return 1
+        }
+
+    echo -n "${os}"
 }
 
 #
